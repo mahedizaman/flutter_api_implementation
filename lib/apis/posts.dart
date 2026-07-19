8ff3c61965ebbb3fcb44411023a1e370f69f1763
mahedizaman <info.zaman.profile@gmail.com>
2026-07-19 11:19:06 +0600

    feat:post data fetching done

--- lib/apis/posts.dart
+++ lib/apis/posts.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as app;
 
 class PostsPage extends StatefulWidget {
   const PostsPage({super.key});
@@ -8,6 +11,25 @@ class PostsPage extends StatefulWidget {
 }
 
 class _PostsPageState extends State<PostsPage> {
+  List posts = [];
+  Future<void> getPosts() async {
+    final res = await app.get(
+      Uri.parse('https://jsonplaceholder.typicode.com/posts'),
+    );
+    if (res.statusCode == 200) {
+      posts = jsonDecode(res.body);
+      setState(() {});
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      getPosts();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,6 +43,34 @@ class _PostsPageState extends State<PostsPage> {
             child: ElevatedButton(onPressed: () {}, child: Text('Next Api')),
           ),
         ],
+      ),
+      body: ListView.builder(
+        itemCount: posts.length,
+        itemBuilder: (context, index) {
+          return Card(
+            surfaceTintColor: const Color.fromARGB(255, 115, 0, 165),
+            color: const Color.fromARGB(255, 104, 139, 96),
+            shadowColor: Colors.amber,
+            margin: const EdgeInsets.all(10),
+            child: Padding(
+              padding: const EdgeInsets.all(12),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    posts[index]['title'],
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Text(posts[index]['body']),
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
